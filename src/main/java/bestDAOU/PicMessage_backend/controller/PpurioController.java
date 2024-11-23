54c1fad956ff7f095876140fd12b49a8a35123cb
안예찬 <yes490411@gmail.com>
2024-11-23 18:56:31 +0900

    feat : 이미지 문자 전송

--- src/main/java/bestDAOU/PicMessage_backend/controller/PpurioController.java
+++ src/main/java/bestDAOU/PicMessage_backend/controller/PpurioController.java
@@ -1,7 +1,7 @@
 package bestDAOU.PicMessage_backend.controller;
 
 import bestDAOU.PicMessage_backend.service.RequestService;
-import bestDAOU.PicMessage_backend.service.RequestService.SendMessageRequest; // RequestService의 SendMessageRequest 사용
+import bestDAOU.PicMessage_backend.service.RequestService.SendMessageRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,9 +17,16 @@ public class PpurioController {
     @Autowired
     private final RequestService requestService;
 
-    // 문자 발송 요청
+    // 메시지와 이미지를 함께 처리하는 요청
     @PostMapping("/send")
-    public List<Map<String, Object>> sendMessages(@RequestBody List<SendMessageRequest> sendMessageRequests) {
-        return requestService.requestSend(sendMessageRequests);
+    public List<Map<String, Object>> sendMessagesWithImage(@RequestBody List<SendMessageRequest> messages) {
+        System.out.println("Received Messages:");
+        for (SendMessageRequest message : messages) {
+            System.out.println("Phone: " + message.getRecipientPhoneNumber());
+            System.out.println("Content: " + message.getMessageContent());
+            System.out.println("Image Base64: " + (message.getImageBase64() != null ? "Exists" : "Null"));
+        }
+        return requestService.requestSendWithImage(messages);
     }
+
 }
